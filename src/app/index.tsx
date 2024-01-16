f2633168ef40d0a783e137aa00cd6e20e7888639
Tugu <115208053+b210930097@users.noreply.github.com>
2024-01-16 08:43:27 +0800

    Make Style header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,12 @@ import {View, Text, StyleSheet} from 'react-native'
 
 const Index = (): JSX.Element =>{
     return(
-        <View style={styles.container}>
+        <View style = {styles.container}>
 
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>Log out</Text>
+            <View style = {styles.header}>
+                <View style = {styles.headerInner}>
+                    <Text style = {styles.headerTitle}>Memo App</Text>
+                    <Text style = {styles.headerRight}>Log out</Text>
                 </View>
             </View>
 
@@ -57,8 +57,30 @@ const Index = (): JSX.Element =>{
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        backgroundColor: "#ffffff"
+    },
+    header:{
+        backgroundColor: "#467FD3",
+        height: 104,
+        justifyContent: 'flex-end'
+    },
+    headerInner:{
+        alignItems: 'center'
+    },
+    headerTitle:{
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    headerRight:{
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+        fontSize: 16,
+        color: 'rgba(255, 255, 255, 0.7)',
+        
     }
 })
 
