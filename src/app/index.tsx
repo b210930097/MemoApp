34750e34399b1f48e19fc99f6de965d93e369e98
Tugu <115208053+b210930097@users.noreply.github.com>
2024-01-16 08:29:27 +0800

    Markup memo list screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,65 @@
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 const Index = (): JSX.Element =>{
     return(
-        <View>
-            <Text>Hello</Text>
+        <View style={styles.container}>
+
+            <View>
+                <View>
+                    <Text>Memo App</Text>
+                    <Text>Log out</Text>
+                </View>
+            </View>
+
+            <View>
+
+                <View>
+                    <View>
+                        <Text>Худалдан авах бүтээгдэхүүний жагсаалт</Text>
+                        <Text>2023.04.01 10:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>Худалдан авах бүтээгдэхүүний жагсаалт</Text>
+                        <Text>2023.04.01 10:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>Худалдан авах бүтээгдэхүүний жагсаалт</Text>
+                        <Text>2023.04.01 10:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+            </View>
+
+            <View>
+                <Text>+</Text>
+            </View>
+
         </View>
+
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
 
 export default Index
